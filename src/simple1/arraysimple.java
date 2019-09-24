1332a284bcea405b51359e67f80569d311391324
Vishram Darshane <vishram.darshane@gmail.com>
2019-09-24 22:30:18 +0530

    add

---
+++ src/simple1/arraysimple.java
@@ -0,0 +1,35 @@
+package simple1;
+
+public class arraysimple {
+	package Array;
+
+	import java.util.Arrays;
+
+	public class arraysimple {
+		public static void main(String[] args) {
+			String ar[]=new String[5];
+			//ar[0]="abc";
+			//ar[1]="vmd";
+			//ar[2]="vak";
+			//ar[3]="nss";
+			//ar[4]="rpb";
+			int ar1[]= {10,85,65,02,56};
+			Arrays.sort(ar1);
+			
+			System.out.println("assending order");
+			for(int i=0;i<ar1.length;i++)
+			{
+			System.out.println(ar1[i]);
+		}
+			System.out.println("Decending Order");
+			for(int i=ar1.length-1;i>=0;i--)
+			{
+			System.out.println(ar1[i]);
+		}
+				
+		}
+	}
+
+	
+
+}
